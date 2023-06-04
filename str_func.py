38e3de6a3ea2f2b4430b366d49973b9f300189a1
Vlad <Vladislav.Mihailovich.91@mail.ru>
2023-06-05 00:38:16 +0400

    add title_str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,11 @@
 def upper_txt(txt):
+    """Функция, которая возвращает все заглавными буквами """
     return txt.upper()
 result = upper_txt("hello world")
+print(result)
+
+def title_txt(txt):
+    """функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return txt.title()
+result = title_txt("hello world")
 print(result)
